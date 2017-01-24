ba2539b41268acc2d157d4aa5f8a2de7e6affd28
amayii0 <rudy.dullier@gmail.com>
2017-01-24 01:21:48 +0100

    Cleanup to sync with readme
    Moved hardcoded values to settings (MQTT server/port, devices, commands,
    topics prefixes, subscribed topics)
    Introduced getFromDic helper function
    Some minor cleaning

--- mqttTranslator.py
+++ mqttTranslator.py
@@ -2,6 +2,7 @@
 # MQTT message translator
 # This script is used to pass messages between Homie devices and PiDome server
 #-------------------------------------------------------------------------------
+# 2017-01-23, RDU: Can echo Homie messages to PiDome and command from PiDome to Homie
 # 2017-01-22, RDU: Initial draft
 ################################################################################
 #
@@ -16,9 +17,17 @@
 ################################################################################
 
 
+
 ################################################################################
 # Settings
 verbose = False
+mqtt_server = "192.168.0.26"
+mqtt_port = 1883
+
+devices = {
+    "5ccf7fd3945b": "17",
+    "17": "5ccf7fd3945b"
+  }
 
 commands = {
     "temperature/degrees": "dht/temp",
@@ -26,10 +35,15 @@ commands = {
     "LED/on": "switch/on/set"
   }
 
-devices = {
-    "5ccf7fd3945b": "17",
-    "17": "5ccf7fd3945b"
-  }
+topicPrefixHomie = "homie/"
+topicPrefixPiDome = "/hooks/devices/"
+
+topics = {
+  topicPrefixHomie + "+/temperature/degrees",
+  topicPrefixHomie + "+/humidity/relative",
+  topicPrefixPiDome + "+/LED/on"
+}
+
 
 
 ################################################################################
@@ -40,39 +54,28 @@ import os
 import re
 
 
+
 ################################################################################
 # Actual translation logic
 def mapDevice(deviceID):
-  if deviceID in devices:
-    return devices[deviceID]
-  else:
-    if verbose:
-      print "Unknown device ID:" + str(deviceID)
-    return "0" # Unknown
+  return getFromDic(devices, deviceID, "0")
 
 def mapCommand(cmd):
-  # Homie: for getters, we need only node ID and advertised property
-  #        for setters, we need to append a /set to topic
-  if cmd in commands:
-    return commands[cmd]
-  else:
-    if verbose:
-      print "Tried to map unknown cmd:" + str(cmd)
-    return "" # Not found
+  return getFromDic(commands, cmd, "")
 
 def translateTopicFromPiDomeToHomieNode(mqttc, msg):
   topicParts = re.split('/', msg.topic)
   dumpTopicParts(topicParts, "    > ")
   deviceID = mapDevice(topicParts[3])
   cmd = mapCommand(topicParts[4] + "/" + topicParts[5])
-  return "homie/" + deviceID + "/" + cmd 
+  return topicPrefixHomie + deviceID + "/" + cmd 
 
 def translateTopicFromHomieNodeToPiDome(mqttc, msg):
   topicParts = re.split('/', msg.topic)
   dumpTopicParts(topicParts, "    > ")
   deviceID = mapDevice(topicParts[1])
   cmd = mapCommand(topicParts[2] + "/" + topicParts[3])
-  return "/hooks/devices/" + deviceID + "/" + cmd
+  return topicPrefixPiDome + deviceID + "/" + cmd
 
 def translateTopic(mqttc, msg):
   # Let's dump some details
@@ -82,13 +85,15 @@ def translateTopic(mqttc, msg):
     print("  > QOS    : " + str(msg.qos))
     print("  > PAYLOAD: " + str(msg.payload))
 
+  # Check if we need to translate a topic received from Homie or PiDome
   res = "Unhandled: " + str(msg.topic)      
-  if msg.topic.startswith("homie/"):
+  if msg.topic.startswith(topicPrefixHomie):
     res = translateTopicFromHomieNodeToPiDome(mqttc, msg)
-  if msg.topic.startswith("/hooks/devices/"):
+  if msg.topic.startswith(topicPrefixPiDome):
     res = translateTopicFromPiDomeToHomieNode(mqttc, msg)
 
-  return res # Not handled
+  return res
+
 
 
 ################################################################################
@@ -96,10 +101,21 @@ def translateTopic(mqttc, msg):
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
     
+    
 def dumpTopicParts(parts, printPrefix):
   if verbose:
     for idx, val in enumerate(parts):
       print str(printPrefix) + "[" + str(idx) + "] " + str(val)
+      
+      
+def getFromDic(dic, key, default):
+  if key in dic:
+    return dic[key]
+  else:
+    if verbose:
+      print "Unknown key:" + str(key)
+    return default # Not found
+
 
 
 ################################################################################
@@ -107,8 +123,9 @@ def dumpTopicParts(parts, printPrefix):
 def on_connect(mqttc, obj, flags, rc):
   print("rc: " + str(rc))
 
+
 def on_message(mqttc, userdata, msg):
-  print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+  print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
   translatedTopic = translateTopic(mqttc, msg)
   if verbose:
     print("Translated topic: " + str(translatedTopic))
@@ -119,23 +136,27 @@ def on_message(mqttc, userdata, msg):
   else:
     mqttc.publish(translatedTopic, msg.payload)
 
+
 def on_publish(mqttc, obj, mid):
   print("mid: " + str(mid))
 
+
 def on_subscribe(mqttc, obj, mid, granted_qos):
-  print("Subscribed: "+str(mid)+" "+str(granted_qos))
+  print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
 
 def on_log(mqttc, obj, level, string):
   print(string)
+
 
 
 ################################################################################
 # Subscribe to topics we want to translate
 # Note that in the case of a SETTER from PiDome to HomieNode, we won't translate the HomieNode topic again
 def subscribeToTopics(mqttc):
-  mqttc.subscribe("homie/+/temperature/degrees", 0)
-  mqttc.subscribe("homie/+/humidity/relative", 0)
-  mqttc.subscribe("/hooks/devices/+/LED/on", 0)
+  for val in topics:
+    mqttc.subscribe(val, 0)
+
 
 
 ################################################################################
@@ -147,14 +168,14 @@ def mainProcess():
 
   # Setup MQTT client  
   mqttc = mqtt.Client()
-  mqttc.on_message = on_message
   mqttc.on_connect = on_connect
-  mqttc.on_publish = on_publish
+  mqttc.on_message = on_message # Required!
+  #mqttc.on_publish = on_publish
   mqttc.on_subscribe = on_subscribe
+  #mqttc.on_log = on_log # For debug only
   
-  # Uncomment to enable debug messages
-  mqttc.on_log = on_log
-  mqttc.connect("192.168.0.26", 1883, 60)
+  # Let's do our "magic"
+  mqttc.connect(mqtt_server, mqtt_port, 60)
   subscribeToTopics(mqttc)
   mqttc.loop_forever() # Really; we don't want to leave
 
